90bc063df68d9db61347daf298b04c6bbd2a9f9b
Haftan666 <j.haftan666@gmail.com>
2024-12-29 00:19:52 +0100

    new game dialog
    does work how I intended it to do, however useless for now (no serialisation, no going into gamescreen etc.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:darttracker/utils/dialog_utils.dart';
 import 'package:darttracker/views/widgets/dart_board/dartboard.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -17,7 +18,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Dart Tracker'),
+        title: const Text('Darts Tracker'),
         centerTitle: true,
         backgroundColor: theme.appBarTheme.backgroundColor,
         actions: [
@@ -33,25 +34,49 @@ class _HomeScreenState extends State<HomeScreen> {
         color: theme.scaffoldBackgroundColor,
         child: Column(
           children: [
+            const SizedBox(height: 20),
+            const Dartboard(),
+            const SizedBox(height: 30),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  showNewGameDialog(context);
+                },
+                style: ElevatedButton.styleFrom(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                  textStyle: const TextStyle(fontSize: 20),
+                  minimumSize: const Size(200, 60),
+                ),
+                child: const Text('New Game'),
+              ),
+            ),
+            const SizedBox(height: 10),
             Center(
               child: ElevatedButton(
                 onPressed: () {},
-                child: const Text('Zlicz punkty'),
+                style: ElevatedButton.styleFrom(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                  textStyle: const TextStyle(fontSize: 20),
+                  minimumSize: const Size(200, 60),
+                ),
+                child: const Text('Game History'),
               ),
             ),
+            const SizedBox(height: 10),
             Center(
               child: ElevatedButton(
                 onPressed: () {},
-                child: const Text('Stwórz party'),
+                style: ElevatedButton.styleFrom(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                  textStyle: const TextStyle(fontSize: 20),
+                  minimumSize: const Size(200, 60),
+                ),
+                child: const Text('Darts Rules'),
               ),
             ),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text('Zobacz historię'),
-              ),
-            ),
-            Dartboard(),
           ],
         ),
       ),
