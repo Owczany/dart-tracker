8f29db0f0245bb742f14cd5593c0c04213fedee0
Piotr Pijanowski <piotr.pijanowski5@gmail.com>
2024-12-28 15:36:35 +0100

    Zrobiony statyczny wygląd tablicy do darta
    Mam wrażenie, że bardzo fajnie wygląda ta tablica, jakby coś się wam nie podobało, to dajcie znać na messengerze. Jeszcze dużo roboty z tą tablicą, ponieważ trzeba będzie zrobić, że jest interaktywna, ale to w swoim czasie, więc na spokojnie. Na razie możecie tam stworzyć samą mechanikę, tworzenia rozgrywki i dodawanie graczy. W sensie użytkownik naciska przycisk stwórz grę dodaje imiona i naciska zacznij. i wtedy będzie widok z tablicą i nazwą gracza

--- lib/views/widgets/dart_board/dartboard.dart
+++ lib/views/widgets/dart_board/dartboard.dart
@@ -21,93 +21,119 @@ class Dartboard extends StatelessWidget {
 class DartBoardPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
+    // Kolory
+    // const Color bullseyeColor = Color;
+
+
     // Środek i promień tarczy
     final Offset center = Offset(0.5 * size.width, 0.5 * size.height);
     final double boardRadius = min(size.width, size.height) / 2;
-    final double innerRadius = boardRadius * 0.7;
-    final double bullseyeRadius = boardRadius * 0.1;
-    final double sectorWidth = boardRadius * 0.05;
+    final double innerRadius = boardRadius * 0.65;
+    final double bullseyeRadius = boardRadius * 0.125;
+    final double sectorWidth = boardRadius * 0.1;
+
+    final rectangle = Rect.fromCircle(center: center, radius: boardRadius);
+
+    final innerRect = Rect.fromCircle(center: center, radius: innerRadius - 0.5 * sectorWidth);
+    final outerRect = Rect.fromCircle(center: center, radius: boardRadius - 0.5 * sectorWidth);
 
     // Paints
+    final backGroundColor = Paint()..color = Colors.white;
+
+    final segmentPaint = Paint()..color = Colors.grey.shade800;
+
+    final sectorPoint = Paint()
+      ..color = Colors.red
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 10.0;
+
+    final innerBullseyePaint = Paint()..color = Colors.red;
+    final outerBullseyePaint = Paint()..color = Colors.green;
+
     final wirePaint = Paint()
       ..color = Colors.grey.shade600
       ..style = PaintingStyle.stroke
       ..strokeWidth = 2.0;
 
+    // TODO: Do zmiany połozenie
     const numberOfSegments = 20;
     const startAngle = -(11 / numberOfSegments) * pi;
     const sweepAngle = 2 * pi / 20;
 
-    // Paints
-    final rectangle = Rect.fromCircle(center: center, radius: boardRadius);
     // Tło tarczy
-    final backGroundColor = Paint()..color = Colors.white;
     canvas.drawCircle(center, boardRadius, backGroundColor);
 
-    // Jeden taki trójkącik
-    final trainglePaint = Paint()..color = Colors.grey.shade800;
-    final aPaint = Paint()
-      ..color = Colors.red
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 10.0;
-
+    // Kolory tarczy
     for (int i = 0; i < 20; i += 2) {
       canvas.drawArc(rectangle, startAngle + (sweepAngle * i), sweepAngle, true,
-          trainglePaint);
-    }
-
-    trainglePaint.color = Colors.amberAccent;
-    aPaint.color = Colors.blue;
-    for (int i = 1; i < 20; i += 2) {
-      canvas.drawArc(rectangle, startAngle + (sweepAngle * i), sweepAngle, true,
-          trainglePaint);
+          segmentPaint);
 
       canvas.drawPath(
-          Path()..addArc(rectangle, startAngle, sweepAngle),
+          Path()..addArc(outerRect, startAngle + (sweepAngle * i), sweepAngle),
           Paint()
             ..color = Colors.red
             ..style = PaintingStyle.stroke
-            ..strokeWidth = 20.0);
-      // canvas.drawPath(Path().., Paint());
+            ..strokeWidth = sectorWidth);
+
+      canvas.drawPath(
+          Path()..addArc(innerRect, startAngle + (sweepAngle * i), sweepAngle),
+          Paint()
+            ..color = Colors.red
+            ..style = PaintingStyle.stroke
+            ..strokeWidth = sectorWidth);
+
+      canvas.drawArc(rectangle, startAngle + (sweepAngle * i), sweepAngle, true,
+          wirePaint);
     }
 
-    trainglePaint.color = Colors.red;
+    segmentPaint.color = Colors.amberAccent;
+    sectorPoint.color = Colors.blue;
+
+    for (int i = 1; i < 20; i += 2) {
+      canvas.drawArc(rectangle, startAngle + (sweepAngle * i), sweepAngle, true,
+          segmentPaint);
+
+      canvas.drawPath(
+          Path()..addArc(outerRect, startAngle + (sweepAngle * i), sweepAngle),
+          Paint()
+            ..color = Colors.green
+            ..style = PaintingStyle.stroke
+            ..strokeWidth = sectorWidth);
+
+      canvas.drawPath(
+          Path()..addArc(innerRect, startAngle + (sweepAngle * i), sweepAngle),
+          Paint()
+            ..color = Colors.green
+            ..style = PaintingStyle.stroke
+            ..strokeWidth = sectorWidth);
+
+      canvas.drawArc(rectangle, startAngle + (sweepAngle * i), sweepAngle, true,
+          wirePaint);
+    }
+
+    segmentPaint.color = Colors.red;
 
     // Środek
-    final innerBullseyePaint = Paint()..color = Colors.red;
-    final outerBullseyePaint = Paint()..color = Colors.green;
-
     canvas.drawCircle(center, bullseyeRadius, outerBullseyePaint);
 
     canvas.drawCircle(center, bullseyeRadius / 2, innerBullseyePaint);
 
-    // Szare druty
-    canvas.drawCircle(
-        center,
-        bullseyeRadius,
-        outerBullseyePaint
-          ..color = Colors.grey.shade600
-          ..style = PaintingStyle.stroke
-          ..strokeWidth = 2.0);
+    // Rysuje szare druty
 
-    canvas.drawCircle(
-        center,
-        bullseyeRadius / 2,
-        innerBullseyePaint
-          ..color = Colors.grey.shade600
-          ..style = PaintingStyle.stroke
-          ..strokeWidth = 2.0);
-
-    canvas.drawCircle(center, boardRadius, wirePaint);
+    // Zewnętrzne druty
     canvas.drawCircle(center, boardRadius - sectorWidth, wirePaint);
 
+    // Wewnętrzne druty
     canvas.drawCircle(center, innerRadius, wirePaint);
     canvas.drawCircle(center, innerRadius - sectorWidth, wirePaint);
 
-    for ( int i = 0; i < 20; i++ ) {
+    // Środkowe druty
+    canvas.drawCircle(center, bullseyeRadius, wirePaint);
+    canvas.drawCircle(center, bullseyeRadius / 2, wirePaint);
+
+    for (int i = 0; i < 20; i++) {
       canvas.clipPath(Path()..lineTo(10.0 * i, 10.0 * i));
     }
-
   }
 
   @override
