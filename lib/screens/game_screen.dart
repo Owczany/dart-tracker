db78ea43469eb215479a8f830080f5fc2cb626bc
Dancer77 <iddqd.idkfa.35@gmail.com>
2024-12-28 23:56:11 +0100

    First version of game screen

---
+++ lib/screens/game_screen.dart
@@ -0,0 +1,73 @@
+import 'package:darttracker/views/widgets/dart_board/dartboard.dart';
+import 'package:flutter/material.dart';
+
+class GameScreen extends StatelessWidget {
+  final String playerName;
+  final int roundNumber;
+
+  GameScreen({required this.playerName, required this.roundNumber});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(      //kolor tła zrobić wg motywu (ciemny/jasny)
+      appBar: AppBar(
+        title: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Text(
+            'Runda $roundNumber',
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 0, left: 16, right: 16, bottom: 16),
+            child: Text(
+              playerName,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ),
+          //const SizedBox(height: 16.0),
+          const Expanded(
+            child: Center(
+              child: Dartboard(),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    // Tu dodać logikę cofania
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,    //dorobić kolorki
+                    foregroundColor: Colors.black,
+                    minimumSize: Size(150, 50),
+                  ),
+                  child: Text('Cofnij'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Tu dodać logikę potwierdzania
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,  //tu też
+                    foregroundColor: Colors.black,
+                    minimumSize: Size(150, 50),
+                  ),
+                  child: Text('Potwierdź'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
