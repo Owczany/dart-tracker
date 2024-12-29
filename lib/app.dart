cc8550b422dd7ee8c785fa07329ed44399de022e
Piotr Pijanowski <piotr.pijanowski5@gmail.com>
2024-12-29 19:26:57 +0100

    Dodawanie języka
    Wstęp tworzenia multi językowej apki

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:darttracker/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -7,13 +8,14 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
+      localizationsDelegates: [],
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
