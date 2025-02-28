0942890693d316682eaedebfb88f871464f16c21
Haftan666 <j.haftan666@gmail.com>
2025-02-28 23:16:38 +0100

    removal of unnecessary options in settings
    they are moved to the proper place

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 //TODO: dopisać zasady (M)
-//TODO: provider tablicy
 //TODO: tryb pro (M)
 //TODO: zmienić kolory tarczy na mniej sraczkwate (w zależności od motywu kolorystycznego)
 //TODO: odpowiednie ustawienia przenieść do tworzenia rozgrywki
-//TODO: zapis ustawień
+
 
 import 'package:darttracker/app.dart';
 import 'package:flutter/material.dart';
